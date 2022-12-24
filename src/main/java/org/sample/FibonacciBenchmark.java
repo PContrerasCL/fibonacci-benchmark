4b3b51111ddc879af6d303b597159b5498735dde
Pablo Contreras <pcontreras@roadforge.cl>
2022-12-24 11:28:38 -0300

    second commit

--- src/main/java/org/sample/FibonacciBenchmark.java
+++ src/main/java/org/sample/FibonacciBenchmark.java
@@ -1,38 +1,10 @@
-/*
- * The MIT License
- *
- * Copyright 2018 Maikel Chandika <mkdika@gmail.com>.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package org.sample;
+
+import org.openjdk.jmh.annotations.*;
 
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Warmup;
 
 /**
  * Fibonacci Naive Recursive vs Memoization Recursive vs Bottom-Up vs Stream API
@@ -43,14 +15,12 @@ import org.openjdk.jmh.annotations.Warmup;
  * with Java 8 Stream API.
  *
  * The function is to calculate Fibonacci position N, then return the value.
- *
- * @author Maikel Chandika (mkdika@gmail.com)
  */
 @Fork(warmups = 2, value = 2)
-@Warmup(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
+@Warmup(iterations = 3, time = 2)
 @BenchmarkMode(value = Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.NANOSECONDS)
-@Measurement(iterations = 3, time = 2, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 2)
 public class FibonacciBenchmark {
 
     public int fibNaiveRecursive(int x) {
